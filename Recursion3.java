03518e56cefa8565d8b586804b212fbcd66c065c
Vishal <vishalvdaware2004@gmail.com>
2025-06-24 18:38:06 +0530

    recursion advance

--- Recursion3.java
+++ Recursion3.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Recursion3 {
 
     // permutation of string
@@ -15,16 +17,59 @@ public class Recursion3 {
     // }
 
     // print total no. of paths in matrix for (n,m)
-    public static int countPaths(int i, int j, int n, int m) {
-        if (i == n || j == m) {
-            return 0;
+    // public static int countPaths(int i, int j, int n, int m) {
+    // if (i == n || j == m) {
+    // return 0;
+    // }
+    // if (i == n - 1 && j == m - 1) {
+    // return 1;
+    // }
+    // int downPaths = countPaths(i + 1, j, n, m);
+    // int rightPaths = countPaths(i, j + 1, n, m);
+    // return downPaths + rightPaths;
+    // }
+
+    // place tiles of size 1*m in a floor of size n*m
+    // public static int placeTiles(int n, int m) {
+    // if (n == m) {
+    // return 2;
+    // }
+    // if (n < m) {
+    // return 1;
+    // }
+    // int vertical = placeTiles(n - m, m);
+    // int horizontal = placeTiles(n - 1, m);
+    // return vertical + horizontal;
+
+    // }
+
+    // // no. of ways to call guest in party (n guest)
+    // public static int callGuest(int n) {
+    // if (n <= 1) {
+    // return 1;
+    // }
+    // int ways1 = callGuest(n - 1);
+    // int ways2 = (n - 1) * callGuest(n - 2);
+    // return ways1 + ways2;
+    // }
+
+    // subsets of first n natural no
+    public static void printSubset(ArrayList<Integer> subset) {
+        for (int i = 0; i < subset.size(); i++) {
+            System.out.print(subset.get(i) + " ");
         }
-        if (i == n - 1 && j == m - 1) {
-            return 1;
+        System.out.println();
+    }
+
+    public static void findSubset(int n, ArrayList<Integer> subset) {
+        if (n == 0) {
+            printSubset(subset);
+            return;
         }
-        int downPaths = countPaths(i + 1, j, n, m);
-        int rightPaths = countPaths(i, j + 1, n, m);
-        return downPaths + rightPaths;
+        subset.add(n);
+        findSubset(n - 1, subset);
+        subset.remove(subset.size() - 1);
+        findSubset(n - 1, subset);
     }
 
     public static void main(String[] args) {
@@ -34,9 +79,22 @@ public class Recursion3 {
         // printPer(str, "");
 
         // print total no. of paths in matrix for (n,m)
-        int n = 3, m = 3;
-        int totalPaths = countPaths(0, 0, n, m);
-        System.out.println(totalPaths);
+        // int n = 3, m = 3;
+        // int totalPaths = countPaths(0, 0, n, m);
+        // System.out.println(totalPaths);
+
+        // place tiles of size 1*m in a floor of size n*m
+        // int n = 4, m = 2;
+        // System.out.println(placeTiles(n, m));
+
+        // // no. of ways to call guest in party (n guest)
+        // int n = 4;
+        // System.out.println(callGuest(n));
+
+        // subsets of first n natural no
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubset(n, subset);
 
     }
 
