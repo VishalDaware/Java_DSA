9b60773549dfa92fe77b3d748da032646e2c5f0f
Vishal <vishalvdaware2004@gmail.com>
2025-07-13 11:35:34 +0530

    binary search tree

--- BinaryTreesYT.java
+++ BinaryTreesYT.java
@@ -91,6 +91,51 @@ public class BinaryTreesYT {
         }
     }
 
+    // count of nodes
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftNodes = countNodes(root.left);
+        int rightNodes = countNodes(root.right);
+        return leftNodes + rightNodes + 1;
+    }
+
+    // sum of nodes
+    public static int sumOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+        return leftSum + rightSum + root.data;
+
+    }
+
+    // height of binary tree
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int myheight = Math.max(leftHeight, rightHeight) + 1;
+        return myheight;
+    }
+
+    // diameter of tree (longest path)
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int diaM1 = diameter(root.left);
+        int diaM2 = diameter(root.right);
+        int diaM3 = height(root.left) + height(root.right) + 1;
+        return Math.max(Math.max(diaM2, diaM1), diaM3);
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
@@ -99,7 +144,12 @@ public class BinaryTreesYT {
         // preorder(root);
         // inorder(root);
         // postorder(root);
-        levelOrder(root);
+        // levelOrder(root);
+        // System.out.println(sumOfNodes(root));
+        // System.out.println(sumOfNodes(root));
+        // System.out.println(countNodes(root));
+        // System.out.println(height(root));
+        System.out.println(diameter(root));
 
     }
 
